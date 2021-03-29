d6cbda2c9bc95d9b83e26799cf43866c318406e6
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-03-30 00:38:45 +0530

    first commit

--- FINALYEAR_PROJECT0/nRFl01_0.h
+++ FINALYEAR_PROJECT0/nRFl01_0.h
@@ -2,6 +2,7 @@
 #define NRFL01_0_H_INCLUDED
 
 #include <stdint.h>
+#include <stdbool.h>
 
 /*********************** nRF Register offset addresses ********************/
 #define CONFIG				(uint8_t) 0x00
@@ -32,9 +33,22 @@
 #define RX_PW_P5			(uint8_t) 0x16
 
 #define FIFO_STATUS		(uint8_t) 0x10
+#define DYNPD 				(uint8_t) 0x1C
+#define FEATURE				(uint8_t) 0x1D
 
 
-
-
+/*********************** nRF Register commands ********************/
+#define R_REGISTER 					(uint8_t) 0x00
+#define W_REGISTER					(uint8_t) 0x01
+#define R_RX_PAYLOAD				(uint8_t) 0x61
+#define W_TX_REGISTER 			(uint8_t) 0xA0
+#define FLUSH_TX						(uint8_t) 0xE1
+#define FLUSH_RX 						(uint8_t) 0xE2
+#define REUSE_TX_PL 				(uint8_t) 0xE3
+#define ACTIVATE		 				(uint8_t) 0x50
+#define R_RX_PL_WID					(uint8_t) 0x60
+#define W_ACK_PAYLOAD				(uint8_t) 0xA8
+#define W_TX_PAYLOAD_NOACK	(uint8_t) 0xB0
+#define NOP									(uint8_t) 0xFF
 
 #endif
